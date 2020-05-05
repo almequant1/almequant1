a90056d17380ff0f570282e5ca0daa673dafe682
almequant1 <64815750+almequant1@users.noreply.github.com>
2020-05-04 21:09:45 -0300

    Create meubot.py

---
+++ meubot.py
@@ -0,0 +1,40 @@
+# -*- encoding utf-8 -*-
+import os, requests, time, sys
+
+#DEFINIÇÃO DE VARIÁVEIS
+x=1
+float(BTC)
+float(BTC_balance)
+float(BTCQ)
+float(BTCQ_balance)
+api_key = "a"
+api_secret = "b"
+
+#DEFINIÇÃO DE FUNÇÕES
+def buy_BTCQ
+#requestbuy = https://quantum.atlasquantum.com/api/buy
+#buy BTCQ order a 0.02, usando BTC_balance total
+
+#def sell_BTCQ
+#requestsell = https://quantum.atlasquantum.com/api/sell
+#sell BTCQ order a 0.03, usando BTCQ_balance total
+
+#OPERAÇÃO
+while x==1:
+
+#login/autenticate
+#requestacesso = requests.get('https://quantum.atlasquantum.com/api/oauth/token')
+
+#check BTC_balance
+#requestsaldo = requests.get('https://quantum.atlasquantum.com/api/oauth/token')
+
+#if BTC_balance > 0,
+	#buy_BTCQ()
+
+#check BTCQ_balance
+
+#if BTCQ_balance > 0
+	#sell_BTCQ()
+
+	print("dormindo 1s")
+	time.sleep(1)
